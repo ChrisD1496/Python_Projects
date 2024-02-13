8fcf6c1aea6eddb38c246f65cbfec03fa834e505
ChrisD1496 <dunkelchristopher@gmail.com>
2024-02-13 08:42:27 +0100

    included Ransac_Regression as well as some debugging

--- Regression.py
+++ Regression.py
@@ -2,15 +2,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression, RANSACRegressor, Ridge, Lasso
 from sklearn.decomposition import PCA
 from sklearn.cross_decomposition import PLSRegression
-from sklearn.linear_model import Ridge, Lasso
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 
-class RegressionModel:
+class RegressionModels:
 
     def __init__(self, link):
         """
@@ -50,7 +49,7 @@ class RegressionModel:
         Plot distribution plots for each column in the dataset.
         """
         num_columns = len(self.df.columns)
-        num_rows = (num_columns + 1) // 2  # Ensure the last row is used if odd number of columns
+        num_rows = (num_columns + 1) // 2
 
         fig, axes = plt.subplots(num_rows, 2, figsize=(15, num_rows * 4))
         axes = axes.flatten()
@@ -59,7 +58,6 @@ class RegressionModel:
             sns.histplot(data=self.df, x=column, kde=True, ax=axes[i])
             axes[i].set_title(f'Distribution of {column}')
 
-        # If the number of columns is odd, remove the empty subplot in the last row
         if num_columns % 2 != 0:
             fig.delaxes(axes[-1])
 
@@ -87,7 +85,7 @@ class RegressionModel:
         x_train_pca = pca.fit_transform(train[independent_variables])
         x_test_pca = pca.transform(test[independent_variables])
 
-        regr = linear_model.LinearRegression()
+        regr = LinearRegression()
         y_train = np.asanyarray(train[[dependent_variable]])
         regr.fit(x_train_pca, y_train)
 
@@ -95,10 +93,10 @@ class RegressionModel:
         test_y = np.asanyarray(test[[dependent_variable]])
 
         r2_score_val = r2_score(test_y, y_hat)
-        mean_absolute_error = np.mean(np.absolute(y_hat - test_y))
-        residual_sum_of_square = np.mean((y_hat - test_y) ** 2)
+        mean_absolute_error_val = mean_absolute_error(test_y, y_hat)
+        mean_squared_error_val = mean_squared_error(test_y, y_hat)
 
-        return r2_score_val, mean_absolute_error, residual_sum_of_square
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
 
     def partial_least_squares_regression(self, dependent_variable, independent_variables, n_components=None, test_size=0.2, random_state=42):
         """
@@ -128,11 +126,10 @@ class RegressionModel:
         test_y = np.asanyarray(test[[dependent_variable]])
 
         r2_score_val = r2_score(test_y, y_hat)
-        mean_absolute_error = np.mean(np.absolute(y_hat - test_y))
-        residual_sum_of_square = np.mean((y_hat - test_y) ** 2)
+        mean_absolute_error_val = mean_absolute_error(test_y, y_hat)
+        mean_squared_error_val = mean_squared_error(test_y, y_hat)
 
-        return r2_score_val, mean_absolute_error, residual_sum_of_square
-
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
 
     def ridge_regression(self, dependent_variable, independent_variables, alpha=1.0, test_size=0.2, random_state=42):
         """
@@ -157,15 +154,15 @@ class RegressionModel:
         y = np.asanyarray(train[[dependent_variable]])
         ridge.fit(x, y)
 
-        y_hat = ridge.predict(test[independent_variables])
         test_x = np.asanyarray(test[independent_variables])
+        y_hat = ridge.predict(test_x)
         test_y = np.asanyarray(test[[dependent_variable]])
 
         r2_score_val = r2_score(test_y, y_hat)
-        mean_absolute_error = np.mean(np.absolute(y_hat - test_y))
-        residual_sum_of_square = np.mean((y_hat - test_y) ** 2)
+        mean_absolute_error_val = mean_absolute_error(test_y, y_hat)
+        mean_squared_error_val = mean_squared_error(test_y, y_hat)
 
-        return r2_score_val, mean_absolute_error, residual_sum_of_square
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
 
     def lasso_regression(self, dependent_variable, independent_variables, alpha=1.0, test_size=0.2, random_state=42):
         """
@@ -190,15 +187,15 @@ class RegressionModel:
         y = np.asanyarray(train[[dependent_variable]])
         lasso.fit(x, y)
 
-        y_hat = lasso.predict(test[independent_variables])
         test_x = np.asanyarray(test[independent_variables])
+        y_hat = lasso.predict(test_x)
         test_y = np.asanyarray(test[[dependent_variable]])
 
         r2_score_val = r2_score(test_y, y_hat)
-        mean_absolute_error = np.mean(np.absolute(y_hat - test_y))
-        residual_sum_of_square = np.mean((y_hat - test_y) ** 2)
+        mean_absolute_error_val = mean_absolute_error(test_y, y_hat)
+        mean_squared_error_val = mean_squared_error(test_y, y_hat)
 
-        return r2_score_val, mean_absolute_error, residual_sum_of_square
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
 
     def simple_linear_regression(self, dependent_variable, independent_variable, test_size=0.2, random_state=42):
         """
@@ -217,7 +214,7 @@ class RegressionModel:
         """
         train, test = train_test_split(self.df, test_size=test_size, random_state=random_state)
 
-        regr = linear_model.LinearRegression()
+        regr = LinearRegression()
         train_x = np.asanyarray(train[[independent_variable]])
         train_y = np.asanyarray(train[[dependent_variable]])
         regr.fit(train_x, train_y)
@@ -227,10 +224,10 @@ class RegressionModel:
         test_y_ = regr.predict(test_x)
 
         r2_score_val = r2_score(test_y, test_y_)
-        mean_absolute_error = np.mean(np.absolute(test_y_ - test_y))
-        residual_sum_of_square = np.mean((test_y_ - test_y) ** 2)
+        mean_absolute_error_val = mean_absolute_error(test_y, test_y_)
+        mean_squared_error_val = mean_squared_error(test_y, test_y_)
 
-        return r2_score_val, mean_absolute_error, residual_sum_of_square
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
 
     def multiple_regression(self, dependent_variables, independent_variable, test_size=0.2, random_state=42):
         """
@@ -248,16 +245,16 @@ class RegressionModel:
         """
         train, test = train_test_split(self.df, test_size=test_size, random_state=random_state)
 
-        regr = linear_model.LinearRegression()
+        regr = LinearRegression()
         x = np.asanyarray(train[dependent_variables])
         y = np.asanyarray(train[[independent_variable]])
         regr.fit(x, y)
 
-        y_hat = regr.predict(test[dependent_variables])
         test_x = np.asanyarray(test[dependent_variables])
+        y_hat = regr.predict(test_x)
         test_y = np.asanyarray(test[[independent_variable]])
 
-        residual_sum_of_squares = np.mean((y_hat - test_y) ** 2)
+        residual_sum_of_squares = mean_squared_error(test_y, y_hat)
         variance_score = regr.score(x, y)
 
         return residual_sum_of_squares, variance_score
@@ -280,23 +277,57 @@ class RegressionModel:
         """
         train, test = train_test_split(self.df, test_size=test_size, random_state=random_state)
 
-        train_x = np.asanyarray(train[['independent_variable']])
-        train_y = np.asanyarray(train[['dependent_variable']])
+        train_x = np.asanyarray(train[[independent_variable]])
+        train_y = np.asanyarray(train[[dependent_variables]])
 
-        test_x = np.asanyarray(test[['independent_variable']])
-        test_y = np.asanyarray(test[['dependent_variable']])
+        test_x = np.asanyarray(test[[independent_variable]])
+        test_y = np.asanyarray(test[[dependent_variables]])
 
         poly = PolynomialFeatures(degree=degree)
         train_x_poly = poly.fit_transform(train_x)
 
-        clf = linear_model.LinearRegression()
-        train_y_ = clf.fit(train_x_poly, train_y)
+        clf = LinearRegression()
+        clf.fit(train_x_poly, train_y)
 
         test_x_poly = poly.transform(test_x)
         test_y_ = clf.predict(test_x_poly)
 
         r2_score_val = r2_score(test_y, test_y_)
-        mean_absolute_error = np.mean(np.absolute(test_y_ - test_y))
-        residual_sum_of_square = np.mean((test_y_ - test_y) ** 2)
+        mean_absolute_error_val = mean_absolute_error(test_y, test_y_)
+        mean_squared_error_val = mean_squared_error(test_y, test_y_)
 
-        return r2_score_val, mean_absolute_error, residual_sum_of_square
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
+
+    def ransac_regression(self, dependent_variables, independent_variable, test_size=0.2, random_state=42):
+        """
+        Perform Polynomial Regression with RANSAC.
+
+        Parameters:
+        - dependent_variables (list): List of column names of dependent variables.
+        - independent_variable (str): The column name of the independent variable.
+        - test_size (float): The proportion of the dataset to include in the test split.
+        - random_state (int): Seed for random number generation for reproducibility.
+
+        Returns:
+        - r2_score_val (float): R-squared score.
+        - mean_absolute_error (float): Mean Absolute Error.
+        - residual_sum_of_square (float): Residual Sum of Squares.
+        """
+        train, test = train_test_split(self.df, test_size=test_size, random_state=random_state)
+
+        train_x = np.asanyarray(train[[independent_variable]])
+        train_y = np.asanyarray(train[dependent_variables])
+
+        test_x = np.asanyarray(test[[independent_variable]])
+        test_y = np.asanyarray(test[dependent_variables])
+
+        ransac = RANSACRegressor(base_estimator=LinearRegression(), random_state=random_state)
+        ransac.fit(train_x, train_y)
+
+        test_y_ = ransac.predict(test_x)
+
+        r2_score_val = r2_score(test_y, test_y_)
+        mean_absolute_error_val = mean_absolute_error(test_y, test_y_)
+        mean_squared_error_val = mean_squared_error(test_y, test_y_)
+
+        return r2_score_val, mean_absolute_error_val, mean_squared_error_val
